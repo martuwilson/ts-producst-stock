7dd1206a689e807f495362f9eb02edb80cd84eef
Martin Williner <williner@ibm.com>
2025-04-21 14:22:44 -0300

    Add colors package for enhanced console output and update logging messages

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 import server from "./server";
+import colors from "colors";
 
 
 server.listen(4001, () => {
-  console.log("Server is running on port 4001");
+  console.log(colors.cyan.bold("Server is running on port 4001"));
 });
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import productsRouter from "./router";
 import db from "./config/db";
+import colors from "colors";
 const server = express();
 
 //Connect to the database
@@ -8,10 +9,10 @@ async function connectToDatabase() {
     try {
         await db.authenticate();
         db.sync();
-        console.log("Database connection has been established successfully.");
+        console.log(colors.bgGreen("Database connection has been established successfully."));
     } catch (error) {
         console.error("Error connecting to the database:", error);
-        console.log("We have an error");
+        console.log(colors.bgRed("Database connection failed"));
     }
 }
 
