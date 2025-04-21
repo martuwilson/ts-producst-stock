35bd2df5ff10a01132603748748010f34c171172
Martin Williner <williner@ibm.com>
2025-04-21 16:27:59 -0300

    Add getProducts handler to retrieve all products and update router to use it

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -2,7 +2,18 @@ import { Request, Response } from "express";
 
 import Product from "../models/Product.model";
 
-const createProduct = async (req: Request, res: Response) => {
+export const getProducts = async (req: Request, res: Response) => {
+    try {
+        const products = await Product.findAll();
+        res.status(200).json(products);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+export const createProduct = async (req: Request, res: Response) => {
 
   const { name, price, availability } = req.body;
 
@@ -14,6 +25,3 @@ const createProduct = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
-
-
-export { createProduct };
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { createProduct } from "./handlers/product";
+import { getProducts, createProduct } from "./handlers/product";
 import { body } from "express-validator";
 import { handleInputError } from "./middlewares";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello from the products route");
-});
+
+router.get("/", getProducts);
+
+
 router.post("/", 
     body("name").notEmpty().withMessage("Name cannot be empty"),
     body("price").isFloat({ gt: 0 }).withMessage("Price must be a positive number"),
